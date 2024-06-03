f097c6b63b88a9b903f41e58e42065b8bfa2bb5e
JaraPava <alberto-11111@live.com.mx>
2024-06-03 07:45:58 -0600

    Se intento consumir api del desarrollador Daniel pero tiene problemas de cors.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'admixion';
+  constructor(private http: HttpClient) {
+    this.fetchData();
+  }
+  fetchData() {
+    this.http.get('https://sisgeact-dev.deptoactext.com/api/v1/periodos/').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
+  }
 }
