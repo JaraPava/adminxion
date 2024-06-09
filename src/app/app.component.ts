b21597447ae6bc24098ddb3fe98db1badf7d72f9
JaraPava <alberto-11111@live.com.mx>
2024-06-08 18:53:31 -0600

    Se añadio una interface hardcodeada en el archivo app.component.ts en y se hizo uso de la variable response de tipo Response que almacena la respuesta de la petición al API Sisgeact através del Interface creada. Y además se imprimio con el elemento de lista desordenada con un NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Response {
+    status: number;
+    data:   Data[];
+}
+
+interface Data {
+    id:      number;
+    periodo: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
+
+
 export class AppComponent {
   title = 'admixion';
   constructor(private http: HttpClient) {
     this.fetchData();
   }
+
+
+  //Guardar la respuesta de la API en una variable
+  response:Response = {status: 0, data: []};
+
   fetchData() {
     this.http.get('https://sisgeact-dev.deptoactext.com/api/v1/periodos/').subscribe({
       next: (response) => {
-        console.log(response);
+        // Obtener la data de la respuesta de esta api
+        this.response = response as Response;
+        console.log('Response:', this.response);
       },
       error: (error) => {
         console.error('Error:', error);
